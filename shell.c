3aac04c6ee3086c5c6a0d318535e9cf73e28c6bc
rabea14180-max <rabea1480@gmail.com>
2025-12-21 14:59:33 +0000

    Add simple shell with execve and Betty style

--- shell.c
+++ shell.c
@@ -3,7 +3,6 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
-#include "hsh.h"
 /**
  * main - simple UNIX command line interpreter
  * @ac: argument count (unused)
@@ -11,14 +10,14 @@
  *
  * Return: Always 0
  */
-
 int main(int ac, char **av)
 {
-char *line = NULL;
+char *line = NULL, *cmd;
 size_t len = 0;
 ssize_t nread;
 pid_t pid;
 char *args[2];
+
 (void)ac;
 while (1)
 {
@@ -27,29 +26,33 @@ write(STDOUT_FILENO, "#cisfun$ ", 9);
 nread = getline(&line, &len, stdin);
 if (nread == -1)
 break;
-if (line[nread - 1] == '\n')
-line[nread - 1] = '\0';
-if (*line == '\0')
-continue;
-
-		pid = fork();
-		if (pid == 0)
-		{
-			args[0] = line;
-			args[1] = NULL;
-			execve(line, args, environ);
-
-			write(STDERR_FILENO, av[0], strlen(av[0]));
-			write(STDERR_FILENO,
-			      ": No such file or directory\n", 28);
-			exit(127);
-		}
-		else
-			wait(NULL);
-	}
-	free(line);
-	if (isatty(STDIN_FILENO))
-		write(STDOUT_FILENO, "\n", 1);
-	return (0);
+cmd = line;
+while (*cmd == ' ' || *cmd == '\t')
+cmd++;
+nread = strlen(cmd);
+while (nread > 0 &&
+(cmd[nread - 1] == ' ' || cmd[nread - 1] == '\t'))
+{
+cmd[nread - 1] = '\0';
+nread--;
 }
-
+if (*cmd == '\0')
+continue;
+pid = fork();
+if (pid == 0)
+args[0] = cmd;
+args[1] = NULL;
+execve(cmd, args, environ);
+write(STDERR_FILENO, av[0], strlen(av[0]));
+write(STDERR_FILENO,
+": No such file or directory\n", 28);
+exit(127);
+}
+else
+wait(NULL);
+}
+free(line);
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "\n", 1);
+return (0);
+}
