705ab62068dd847d5af2682d64d8717e0108bcce
rabea14180-max <rabea1480@gmail.com>
2025-12-21 15:12:34 +0000

    Add simple shell with execve and Betty style

--- shell.c
+++ shell.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include "hsh.h"
 /**
  * main - simple UNIX command line interpreter
  * @ac: argument count (unused)
@@ -12,47 +13,58 @@
  */
 int main(int ac, char **av)
 {
-char *line = NULL, *cmd;
-size_t len = 0;
-ssize_t nread;
-pid_t pid;
-char *args[2];
+	char *line = NULL, *cmd;
+	size_t len = 0;
+	ssize_t nread;
+	pid_t pid;
+	char *args[2];
 
-(void)ac;
-while (1)
-{
-if (isatty(STDIN_FILENO))
-write(STDOUT_FILENO, "#cisfun$ ", 9);
-nread = getline(&line, &len, stdin);
-if (nread == -1)
-break;
-cmd = line;
-while (*cmd == ' ' || *cmd == '\t')
-cmd++;
-nread = strlen(cmd);
-while (nread > 0 &&
-(cmd[nread - 1] == ' ' || cmd[nread - 1] == '\t'))
-{
-cmd[nread - 1] = '\0';
-nread--;
+	(void)ac;
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
+
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+			break;
+
+		cmd = line;
+		while (*cmd == ' ' || *cmd == '\t')
+			cmd++;
+
+		nread = strlen(cmd);
+		while (nread > 0 &&
+		      (cmd[nread - 1] == ' ' || cmd[nread - 1] == '\t'))
+		{
+			cmd[nread - 1] = '\0';
+			nread--;
+		}
+
+		if (*cmd == '\0')
+			continue;
+
+		pid = fork();
+		if (pid == 0)
+		{
+			args[0] = cmd;
+			args[1] = NULL;
+			execve(cmd, args, environ);
+
+			write(STDERR_FILENO, av[0], strlen(av[0]));
+			write(STDERR_FILENO, ": No such file or directory\n", 28);
+			exit(127);
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+
+	free(line);
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "\n", 1);
+
+	return (0);
 }
-if (*cmd == '\0')
-continue;
-pid = fork();
-if (pid == 0)
-args[0] = cmd;
-args[1] = NULL;
-execve(cmd, args, environ);
-write(STDERR_FILENO, av[0], strlen(av[0]));
-write(STDERR_FILENO,
-": No such file or directory\n", 28);
-exit(127);
-}
-else
-wait(NULL);
-}
-free(line);
-if (isatty(STDIN_FILENO))
-write(STDOUT_FILENO, "\n", 1);
-return (0);
-}
+
