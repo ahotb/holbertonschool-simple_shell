340507c01a0e9b7f7fd44437956a87ddf4529b8e
ahotb <ah.r9625@gmail.com>
2025-12-22 19:03:52 +0300

    fixed

--- execute.c
+++ execute.c
@@ -24,7 +24,7 @@ void execute_command(char **args, char **av)
 		write(STDERR_FILENO, ": 1: ", 5);
 		write(STDERR_FILENO, args[0], strlen(args[0]));
 		write(STDERR_FILENO, ": not found\n", 12);
-		return (127);
+		return;
 	}
 
 	while (args[i] != NULL && i < 63)
