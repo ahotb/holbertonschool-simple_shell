ec59c8b1eeb5522ed126702d9653b0953cdd944e
ahotb <ah.r9625@gmail.com>
2025-12-23 17:53:05 +0300

    Modifying variables in the trim_spaces function

--- parse.c
+++ parse.c
@@ -19,7 +19,7 @@ char *trim_spaces(char *str)
 	if (*str == '\0')
 		return (str);
 
-	end = str + strlen(str) - 1;
+	end = str + _strlen(str) - 1;
 	while (end > str && (*end == ' ' || *end == '\t'))
 		end--;
 
