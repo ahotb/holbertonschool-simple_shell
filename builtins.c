f6ff5eb0cc4b12f220bd230f72395e09fb46b64e
ahotb <ah.r9625@gmail.com>
2025-12-23 11:56:28 +0300

    fiexd

--- builtins.c
+++ builtins.c
@@ -9,9 +9,13 @@
 int is_builtin(char **args)
 {
 	if (!args || !args[0])
+	{
 		return (0);
+	}
 	if (strcmp(args[0], "exit") == 0)
+	{
 		return (1);
+	}
 	if (strcmp(args[0], "env") == 0)
 	{
 		return (1);
