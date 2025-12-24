91f02388b0f50d7c5a340af21524c7d42e73f101
rabea14180-max <rabea14180@gmail.com>
2025-12-24 13:52:36 +0000

    exit 100 and negative

--- shell.c
+++ shell.c
@@ -30,24 +30,24 @@ void shell_loop(char **av)
 
 		if (*line)
 		{
-			args = tokenize(line);
-			if (args && is_builtin(args))
-			{
-				last_status = handle_builtin(args, av[0], last_status);
-				if (last_status < 0)
-				{
-					free_tokens(args);
-					free(line);
-					exit((-last_status) - 1);
-				}
-			}
-			else if (args)
-				last_status = execute_command(args, av);
 
-			free_tokens(args);
-		}
-		line = orig;
-	}
-		if(line)
-		free(line);
+		args = tokenize(line);
+            if (args)
+            {
+                if (is_builtin(args))
+                {
+                    last_status = handle_builtin(args, av[0], last_status);
+                }
+                else
+                {
+                    last_status = execute_command(args, av);
+                }
+                free_tokens(args);
+            }
+        }
+        line = orig;
+    }
+
+    if (line)
+        free(line);
 }
