fefbab94aa605eac42788edeb54573b234b9e211
ahotb <ah.r9625@gmail.com>
2025-12-25 17:07:44 +0300

    Re-deal with Handle and exit

--- builtins.c
+++ builtins.c
@@ -21,37 +21,42 @@ int is_builtin(char **args)
 
 int handle_builtin(char **args, char *prog, int last_status)
 {
-	int i = 0;
+	int i = 0, code;
 
 	if (_strcmp(args[0], "exit") == 0)
 	{
-		if (!args[1])
-			 exit(last_status);
-	if (args[1][0] == '-')
-{
-write(2, prog, _strlen(prog));
-write(2, ": 1: exit: Illegal number: ", 27);
-write(2, args[1], _strlen(args[1]));
-write(2, "\n", 1);
-exit(2);
-}
-	while (args[1][i])
-{
-if (args[1][i] < '0' || args[1][i] > '9')
-{
-write(2, prog, _strlen(prog));
-write(2, ": exit: ", 8);
-write(2, args[1], _strlen(args[1]));
-write(2, ": numeric argument required\n", 28);
-exit(2);
-}
-i++;
-}
-exit(_atoi(args[1]) % 256);
-}
+		code = last_status;
+		if (args[1])
+		{
+			if (args[1][0] == '-')
+			{
+				write(2, prog, _strlen(prog));
+				write(2, ": 1: exit: Illegal number: ", 27);
+				write(2, args[1], _strlen(args[1]));
+				write(2, "\n", 1);
+				exit(2);
+			}
+			i = 0;
+			while (args[1][i])
+			{
+				if (args[1][i] < '0' || args[1][i] > '9')
+				{
+					write(2, prog, _strlen(prog));
+					write(2, ": exit: ", 11);
+					write(2, args[1], _strlen(args[1]));
+					write(2, ": numeric argument required\n", 28);
+					exit(2);
+				}
+				i++;
+			}
+			code = (_atoi(args[1]) % 256);
+		}
+		exit(code);
+	}
 
 	if (_strcmp(args[0], "env") == 0)
 	{
+		i = 0;
 		while (environ[i])
 		{
 			write(1, environ[i], _strlen(environ[i]));
